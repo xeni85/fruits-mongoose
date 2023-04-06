90162b2397f5c2dbfc792f4262328421bb735413
xeni85 <shkelzen.dunisha@umsh.edu.al>
2023-04-05 22:14:37 -0400

    building up the schemas

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+require('dotenv').config();
+
+app.set('view engine', 'jsx');
+app.engine('jsx', require('express-react-views').createEngine());
+
+
+
+//---Middleware---
+app.use((req, res, next) => {
+    console.log('I run for all routes');
+    next();
+});
+app.use(express.urlencoded({extended: true}));
+
+
 const mongoose = require('mongoose');
 const Fruit = require('./models/fruit.js');
 
@@ -11,3 +32,45 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 mongoose.connection.once('open', ()=> {
     console.log('connected to mongo');
 });
+
+//---Routing---
+app.get('/fruits/', (req, res) => {
+    res.render('Index', {fruits: fruits});
+});
+app.get('/fruits/new', (req, res) => {
+    res.render('New');
+});
+
+app.post('/fruits', (req, res)=>{
+    if(req.body.readyToEat === 'on'){ //if checked, req.body.readyToEat is set to 'on'
+        req.body.readyToEat = true;
+    } else { //if not checked, req.body.readyToEat is undefined
+        req.body.readyToEat = false;
+    }
+    fruits.push(req.body);
+    res.redirect('/fruits'); //send the user back to /fruits
+});
+
+app.post('/fruits', (req, res) => {
+    res.send(fruits);
+});
+app.post('/fruits/', (req, res)=>{
+    if(req.body.readyToEat === 'on'){ //if checked, req.body.readyToEat is set to 'on'
+        req.body.readyToEat = true;
+    } else { //if not checked, req.body.readyToEat is undefined
+        req.body.readyToEat = false;
+    }
+    Fruit.create(req.body, (error, createdFruit)=>{
+        res.send(createdFruit);
+    });
+});
+
+app.get('/fruits/:indexOfFruitsArray', (req, res) => {
+    res.render('Show', {
+        fruit: fruits[req.params.indexOfFruitsArray]
+    });
+});
+
+
+
+app.listen(PORT, () => {console.log(`Server is running on port ${PORT}`)});
---
+++ views/Index.jsx
@@ -0,0 +1,33 @@
+const React = require('react');
+
+    class Index extends React.Component {
+      render() {
+          const { fruits } = this.props;
+          return (
+                  <div>
+                    <nav>
+                    <a href="/fruits/new">Create a New Fruit</a>
+                    </nav>
+                      <h1>Fruits Index Page</h1>
+                      <ul>
+                          {fruits.map((fruit, i) => {
+                              return (
+                                  <li key={fruit.name}>
+                                      The{' '}
+                                      <a href={`/fruits/${i}`}>
+                                          {fruit.name}
+                                      </a>{' '}
+                                      is {fruit.color} <br></br>
+                                      {fruit.readyToEat
+                                          ? `It is ready to eat`
+                                          : `It is not ready to eat`}
+                                      <br />
+                                  </li>
+                              );
+                          })}
+                      </ul>
+                  </div>
+          );
+      }
+    }
+    module.exports = Index;
